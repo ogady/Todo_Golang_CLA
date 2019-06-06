a3a61b7dcc79fcfe83237208d974419225faaa24
Takumi Ogawa <ogataku01123@gmail.com>
2019-06-06 22:53:54 +0900

    [add]router,sqlhundler,context,hundler

--- usecase/todo.go
+++ usecase/todo.go
@@ -27,9 +27,8 @@ type todoUsecase struct {
 /*
 NewTodoUsecase is constructor for init
 TodoUsecase interface を返却している
-interfaceに構造体の参照渡しで代入することができている？？
 go言語において呼び出し元にinterfaceのみを公開することで実装の詳細を隠ぺいすると、gomockを使ったテストがやり易くなる。
-NewTodoUsecase 関数でのみ todoUsecase のメモリを確保し、interfaceである TodoUsecase 型として利用者に公開します。
+NewTodoUsecase 関数で todoUsecase のメモリを確保し、interfaceである TodoUsecase 型として利用者に公開します。
 */
 func NewTodoUsecase(todoRepo repository.TodoRepository) TodoUsecase {
 	todoUsecase := todoUsecase{todoRepo: todoRepo}
